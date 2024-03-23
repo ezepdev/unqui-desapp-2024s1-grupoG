0a5a0632a26021b9a932950db5ff85625c9f468c
ezepdev <edjpinheiro@gmail.com>
2024-03-22 23:37:35 -0300

    code: initialize the spring project

---
+++ src/main/java/ar/edu/unq/desapp/grupoG/backendapicryptoexchange/BackendApiCryptoexchangeApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoG.backendapicryptoexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiCryptoexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiCryptoexchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/desapp/grupoG/backendapicryptoexchange/BackendApiCryptoexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoG.backendapicryptoexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiCryptoexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
