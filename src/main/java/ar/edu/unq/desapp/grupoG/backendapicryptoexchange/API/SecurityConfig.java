c8f564d22ad93207a4c680dd848b92240fda85df
ezepdev <edjpinheiro@gmail.com>
2024-03-31 12:40:40 -0300

    code: add a basic implementation of spring security configuration

---
+++ src/main/java/ar/edu/unq/desapp/grupoG/backendapicryptoexchange/API/SecurityConfig.java
@@ -0,0 +1,69 @@
+package ar.edu.unq.desapp.grupoG.backendapicryptoexchange.API;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity /* enable security attributes in controllers */
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                /*.authorizeHttpRequests(http -> {
+                        // PUBLIC ENDPOINTS
+                        http.requestMatchers(HttpMethod.GET,"/auth/register").permitAll();
+                        // PRIVATE ENDPOINTS
+                        http.requestMatchers(HttpMethod.GET,"/all").hasAuthority("READ");
+                        // DENY REST ENDPOINTS
+                        http.anyRequest().denyAll();                                            }
+                )*/
+                .build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(null);
+        provider.setUserDetailsService(null);
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+          return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean                                                                                
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("eze").password("1234").roles("ADMIN").authorities("READ","CREATE").build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
