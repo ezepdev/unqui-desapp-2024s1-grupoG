5b81ce1519ab5d0c9961c588be6b697c25d18958
ezepdev <edjpinheiro@gmail.com>
2024-06-24 11:30:15 -0300

    fix logs in AuditionableAspectPointcut

--- src/main/java/ar/edu/unq/desapp/grupog/backendapicryptoexchange/aspects/AuditionableAspectPointcut.java
+++ src/main/java/ar/edu/unq/desapp/grupog/backendapicryptoexchange/aspects/AuditionableAspectPointcut.java
@@ -1,19 +1,16 @@
 package ar.edu.unq.desapp.grupog.backendapicryptoexchange.aspects;
 
 
-import ar.edu.unq.desapp.grupog.backendapicryptoexchange.service.JWTService;
 import io.swagger.v3.core.util.Json;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,7 +20,6 @@ import java.util.Map;
 @Order(0)
 public class AuditionableAspectPointcut {
 
-    static final String[] pointcut = {};
     static final Logger logger = LogManager.getLogger(AuditionableAspectPointcut.class);
 
 
@@ -35,26 +31,21 @@ public class AuditionableAspectPointcut {
     public Object aroundAuditionableMethod(ProceedingJoinPoint pjp) throws Throwable {
         String methodName = pjp.getSignature().getName();
 
-        logger.info("timestamp call method: " + new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(new Date()));
+        if (logger.isInfoEnabled()) logger.info("timestamp call method: {}",  new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(new Date()));
 
-        //logger.info("method call: " + pjp.getSignature().getName());
-        //logger.info("method class with parameters: " + Arrays.toString(pjp.getArgs()));
         logRequest(pjp, pjp.getSignature().toString());
         long startTime = System.currentTimeMillis();
         Object result = pjp.proceed();
         long endTime = System.currentTimeMillis();
-        logger.info("execution time: " + (endTime - startTime) + " ms");
-
-
-        logger.info("\nResponse time: " + (endTime - startTime) + "ms", true);
-        logger.info("<- " + methodName + " Response: \n" + Json.pretty(result) + "\n", true);
+        if (logger.isInfoEnabled()) logger.info("\nexecuted time: {}ms", endTime - startTime);
+        if (logger.isInfoEnabled()) logger.info("<- {} Response: \n{}\n", methodName, Json.pretty(result));
 
         return result;
     }
 
 
     private void logRequest(ProceedingJoinPoint jp, String serviceName) {
-        logger.info(serviceName + " CALL:", true);
+        if (logger.isInfoEnabled()) logger.info("{} CALL:", serviceName);
         String[] argNames = ((MethodSignature) jp.getSignature()).getParameterNames();
         Object[] values = jp.getArgs();
         Map<String, Object> params = new HashMap<>();
@@ -63,19 +54,10 @@ public class AuditionableAspectPointcut {
                 params.put(argNames[i], values[i]);
             }
         }
-        logger.info("-> " + jp.getSignature().getName() + " Request", true);
-        if (!params.isEmpty()) logger.info(Json.pretty(params), true);
+        if (logger.isInfoEnabled() && !params.isEmpty()){
+            logger.info("-> {} Request", jp.getSignature().getName());
+            logger.info("with params: {}",Json.pretty(params));
     }
-
-    @Before("auditionnable()")
-    public void beforeAuditionableMethod() {
-        logger.info("before auditionnable method");
     }
-
-    @After("auditionnable()")
-    public void afterAuditionableMethod() {
-        logger.info("after auditionnable method");
-    }
-
 }
 
