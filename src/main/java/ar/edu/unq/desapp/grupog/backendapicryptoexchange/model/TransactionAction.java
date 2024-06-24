9203022de56d53a9b76cbb2d9e05de422607f4cc
Tobias Romero <arebuayon@hotmail.com>
2024-06-24 14:08:17 -0300

    Test UserModel y CryptoService

--- src/main/java/ar/edu/unq/desapp/grupog/backendapicryptoexchange/model/TransactionAction.java
+++ src/main/java/ar/edu/unq/desapp/grupog/backendapicryptoexchange/model/TransactionAction.java
@@ -3,5 +3,6 @@ package ar.edu.unq.desapp.grupog.backendapicryptoexchange.model;
 public enum TransactionAction {
     CONFIRM_TRANSFER,
     CONFIRM_RECEIPT,
-    CANCEL
+    CANCEL,
+    INVALID_ACTION
 }
